f3919b80874006fdfcba3ddd17b05af0e7e5a0b5
Enhancer <xu.jiahao@darcytech.com>
2020-10-15 10:21:49 +0800

    116 PopulatingNextRightPointersInEachNode using DFS

--- src/main/java/com/example/leetCode/PopulatingNextRightPointersInEachNode.java
+++ src/main/java/com/example/leetCode/PopulatingNextRightPointersInEachNode.java
@@ -1,33 +1,16 @@
 package com.example.leetCode;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class PopulatingNextRightPointersInEachNode {
     public Node connect(Node root) {
-        if (root == null) {
-            return null;
+        if (root != null && root.left != null) {
+            root.left.next = root.right;
+            if (root.next != null) {
+                root.right.next = root.next.left;
+            }
+            connect(root.left);
+            connect(root.right);
         }
 
-        Queue<Node> queue = new LinkedList<>();
-        Queue<Node> nextLevelQueue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            Node node = queue.poll();
-            if (node.left != null) {
-                nextLevelQueue.offer(node.left);
-            }
-            if (node.right != null) {
-                nextLevelQueue.offer(node.right);
-            }
-
-            if (!queue.isEmpty()) {
-                node.next = queue.peek();
-            } else {
-                queue = nextLevelQueue;
-                nextLevelQueue = new LinkedList<>();
-            }
-        }
         return root;
     }
 }
