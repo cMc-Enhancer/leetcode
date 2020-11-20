37a1bda8d6ebd8d4e21220aae7b8975576db0992
Enhancer <xu.jiahao@darcytech.com>
2020-11-20 10:10:58 +0800

    406 QueueReconstructionByHeight failed

---
+++ src/main/java/com/example/leetCode/QueueReconstructionByHeight.java
@@ -0,0 +1,12 @@
+package com.example.leetCode;
+
+// Input:
+// [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+//
+// Output:
+// [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+public class QueueReconstructionByHeight {
+//    public int[][] reconstructQueue(int[][] people) {
+//
+//    }
+}
